92a088e01ac34f5f1104da9fac241961084538a5
ruslan <vergilijus@ya.ru>
2020-08-29 11:18:40 +0300

    tf2 runner

---
+++ scripts/run_train2.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/running_locally.md
+# Usage:
+# run_train.sh <config_path> <model_dir> <num_train_steps>
+
+API_PATH=/home/ruslan/projects/models/research
+
+PIPELINE_CONFIG_PATH=$1
+MODEL_DIR=$2
+NUM_TRAIN_STEPS=$3
+SAMPLE_1_OF_N_EVAL_EXAMPLES=1
+
+mkdir ${MODEL_DIR}
+cp ${PIPELINE_CONFIG_PATH} ${MODEL_DIR}/pipeline.config
+
+python ${API_PATH}/object_detection/model_main_tf2.py \
+    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+    --model_dir=${MODEL_DIR} \
+    --num_train_steps=${NUM_TRAIN_STEPS} \
+    --sample_1_of_n_eval_examples=${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
+    --alsologtostderr
